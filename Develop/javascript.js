40498915d1250e56bc51afd5ec26389d0a9974ad
Will <shaheenwill@gmail.com>
2021-04-22 18:57:50 -0400

    updated local storage

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,5 +1,39 @@
 var currentDate = new Date();
-var $body = $('body');
-var $h6 = $('h6');
-var date = document.getElementById('date')
-date.innerHTML = currentDate
+var date = $('#date');
+date.html(currentDate.toLocaleDateString());
+var hour = currentDate.getHours();
+var id;
+
+if (hour < 12) {
+    id = "am" + hour;
+} else if (hour > 12) {
+    id = "pm" + (hour - 12)
+} else {
+    id = "pm12";
+}
+var row = $("#" + id);
+if (row) {
+    row.addClass("current");
+}
+var rows = $("main div");
+
+$.each(rows, getAppt);
+
+function getAppt(index, row) {
+    var appt = localStorage.getItem(row.id);
+
+    if (appt) {
+        $(row).find("textarea").val(appt);
+    }
+}
+$.each(rows, saveBtn);
+function saveBtn(index, row) {
+    $(row).find("button").click(handleSave);
+}
+function handleSave(){
+    var row = $(this).parent();
+    var id = row.attr("id");
+    var appt = row.find("textarea").val();
+    localStorage.setItem(id,appt);
+         
+}
